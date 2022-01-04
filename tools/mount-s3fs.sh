80a245b6207783f3f3021b07e904f68f60211db5
Vsevolod Ganin <vsevolod.ganin@gmail.com>
2022-01-04 03:01:37 +0300

    Update export domain

--- tools/mount-s3fs.sh
+++ tools/mount-s3fs.sh
@@ -1,1 +1,4 @@
-s3fs dev.vsevolodganin.com s3fs-mount/ -o passwd_file=$HOME/.passwd-s3fs -o url=http://storage.yandexcloud.net -o use_path_request_style
+set -e
+
+mkdir -p s3fs-mount
+s3fs www.vsevolodganin.com s3fs-mount/ -o passwd_file=$HOME/.passwd-s3fs -o url=http://storage.yandexcloud.net -o use_path_request_style
